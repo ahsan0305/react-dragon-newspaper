388520dfd4d235904401b6880f222a736e78f7d8
ahsan0305 <hridoahsanul@gmail.com>
2023-10-03 01:12:19 +0600

    Breaking news slider using React Marquee and navbar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,18 @@
 import Header from "../Shared/Header/Header";
 import LeftSideContent from "../Shared/LeftSideContent/LeftSideContent";
 import Navbar from "../Shared/Navbar/Navbar";
-import RightSideContent from "../Shared/RightSideContant/RightSideContent";
+import RightSideContent from "../Shared/RightSideContent/RightSideContent";
+
+import BreakingNews from "./BreakingNews";
 
 
 const Home = () => {
     return (
-        <div>
-            <Navbar></Navbar>
+        <div className="space-y-5">
+           
             <Header></Header>
+            <BreakingNews></BreakingNews>
+            <Navbar></Navbar>
             
             <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
                 <div>
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -3,8 +3,8 @@ import moment from 'moment';
 
 const Header = () => {
     return (
-        <div>
-            <img src={logo} alt="" />
+        <div className='text-center space-y-4'>
+            <img className='mx-auto' src={logo} alt="" />
             <p className='text-2xl'>Journalism Without Fear or Favour</p>
             <p className='text-xl'>{moment().format("dddd, MMMM, D YYYY, h:mm a")}</p>
         </div>
